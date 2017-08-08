6cdf3e723a7a318229bcd918a107a0512a3dc7dc
zk277 <zuzaka22@hotmail.com>
2017-08-08 23:24:54 +0100

    Tagging feature completed

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  resources :tags
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
