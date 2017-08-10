d605dcc6c6c07350a294522cbbe676ac47625fad
zk277 <zuzaka22@hotmail.com>
2017-08-10 15:26:52 +0100

    Sorcery authentication complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809143805) do
+ActiveRecord::Schema.define(version: 20170809214839) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 20170809143805) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_attachments_on_article_id"
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "email", null: false
+    t.string "crypted_password", null: false
+    t.string "salt", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_authors_on_email", unique: true
   end
 
   create_table "comments", force: :cascade do |t|
